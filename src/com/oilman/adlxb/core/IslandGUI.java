c699db8bd9f65b55d21d80d9e7590e29d8ae4008
Oilman <34388992+manoil@users.noreply.github.com>
2021-03-16 19:46:09 +0800

    增加颜文字选择器
    神秘Java，如果你所有的TextPane都在ScrollPanel里面，那么它就无法显示字体中不包含的文字（不会寻找缺省文字）

--- src/com/oilman/adlxb/core/IslandGUI.java
+++ src/com/oilman/adlxb/core/IslandGUI.java
@@ -1,11 +1,17 @@
 package com.oilman.adlxb.core;
 
 import javax.swing.*;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Objects;
 
 import static com.oilman.adlxb.core.IslandGuiUtils.*;
+import static com.oilman.adlxb.core.IslandSettings.toLog;
 
 /**
  * The desktop GUI version of this program.
@@ -15,13 +21,13 @@ import static com.oilman.adlxb.core.IslandGuiUtils.*;
  * @since 2.1.0
  */
 public class IslandGUI {
-    private JTextField userInputTextField;
     private JButton sendButton;
-    private JScrollPane scrollPane;
     private JPanel jPanel2;
     private JPanel rootJPanel;
-    private JLabel inputHintLabel;
     private JTextPane outputTextPane;
+    private JComboBox comboBox1;
+    private JTextPane userInputTextPane;
+    private JTextPane 要PO些啥呢TextPane;
 
     static Font normalFont;
     static Font boldFont;
@@ -45,7 +51,6 @@ public class IslandGUI {
         }
         setDefaultFontTo(getFont());
 
-
         JFrame frame = new JFrame("A岛离线版");
         frame.setContentPane(new IslandGUI().rootJPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -56,14 +61,38 @@ public class IslandGUI {
 
     private void createUIComponents() {
         sendButton = new JButton();
+        comboBox1 = new JComboBox();
+        comboBox1.addPopupMenuListener(new PopupMenuListener() {
+            @Override
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+                //Do nothing
+            }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+                if (toLog){
+                    System.out.println("Selected: "+Objects.requireNonNull(comboBox1.getSelectedItem()).toString());
+                }
+                userInputTextPane.setText(userInputTextPane.getText()+
+                        Objects.requireNonNull(comboBox1.getSelectedItem()).toString());
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent e) {
+                //Do nothing
+            }
+        });
+
+        comboBox1.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,12));
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (userInputTextField.getText().isBlank()) {
+                if (userInputTextPane.getText().isBlank()) {
                     outputTextPane.setText("输入点东西吧");
                 } else {
-                    IslandThread thisThread = new IslandThread(userInputTextField.getText());
+                    IslandThread thisThread = new IslandThread(userInputTextPane.getText());
                     outputTextPane.setText(thisThread.toString());
+                    userInputTextPane.setText("");
                 }
             }
         });
